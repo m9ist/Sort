91edfcd58c5effd8a8f272b1b21c085d99ea55f3
Oleg <kazmin.oleg@gmail.com>
2013-10-13 14:59:31 +0700

    program sort)

--- src/home/Main.java
+++ src/home/Main.java
@@ -1,8 +1,25 @@
 package home;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> res = new ArrayList<String>();
+        while (sc.hasNextLine())
+        {
+            String s = sc.nextLine();
+            res.add(s);
+        }
+
+        Collections.sort(res);
+
+        for (String i : res)
+        {
+            System.out.println(i);
+        }
     }
 }
